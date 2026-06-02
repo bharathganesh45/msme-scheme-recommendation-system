fdbfc284a3955c2da5b9accfd42e50498640b9fb
bharathganesh45 <bharathganesh45@gmail.com>
2026-06-02 22:50:07 +0530

    connect the database

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,13 +7,16 @@ const { Pool } = pkg;
 
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false }: false
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 
 pool.on('connect', () => {
-    console.log('Connected to Neon Postgres database');
+    console.log('Connected to local Postgres database');
 });
 
 
